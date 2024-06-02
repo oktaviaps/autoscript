53b3afb8b4d9163775cc49eaa916e03a13d9df94
oktaviaps <158389264+oktaviaps@users.noreply.github.com>
2024-06-02 18:14:03 +0700

    Add files via upload

--- update.sh
+++ update.sh
@@ -1,95 +1,14 @@
 #!/bin/bash
+clear
+cd /usr/local/
+rm -rf sbin
+rm -rf /usr/bin/enc
+cd
+mkdir /usr/local/sbin
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
-###########- COLOR CODE -##############
-colornow=$(cat /etc/rmbl/theme/color.conf)
-NC="\e[0m"
-RED="\033[0;31m"
-COLOR1="$(cat /etc/rmbl/theme/$colornow | grep -w "TEXT" | cut -d: -f2|sed 's/ //g')"
-COLBG1="$(cat /etc/rmbl/theme/$colornow | grep -w "BG" | cut -d: -f2|sed 's/ //g')"
-WH='\033[1;37m'
-###########- END COLOR CODE -##########
-echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
-echo -e "$COLOR1 ${NC} ${COLBG1}                 ${WH}⇱ UPDATE ⇲                    ${NC} $COLOR1 $NC"
-echo -e "$COLOR1 ${NC} ${COLBG1}             ${WH}⇱ SCRIPT TERBARU ⇲                ${NC} $COLOR1 $NC"
-echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
-
-
-#hapus menu
-rm -rf restore
-rm -rf m-trgo
-rm -rf backup
-rm -rf menu
-rm -rf addnoobz
-rm -rf cek-noobz
-rm -rf m-noobz
-rm -rf m-vmess
-rm -rf m-vless
-rm -rf m-trojan
-rm -rf m-system
-rm -rf m-sshovpn
-rm -rf m-ssws
-rm -rf running
-rm -rf m-update
-rm -rf m-backup
-rm -rf m-theme
-rm -rf m-ip
-rm -rf m-bot
-rm -rf update
-rm -rf ws-dropbear
-rm -rf bckpbot
-rm -rf tendang
-rm -rf bottelegram
-rm -rf restore
-rm -rf backup
-rm -rf cleaner
-rm -rf m-allxray
-rm -rf xraylimit
-rm -rf xp
-rm -rf trialvmess
-rm -rf trialvless
-rm -rf trialtrojan
-rm -rf trialssh
-rm -rf bantwidth
-
-# download menu
-cd /usr/bin
-rm -rf restore
-rm -rf m-trgo
-rm -rf backup
-rm -rf menu
-rm -rf addnoobz
-rm -rf cek-noobz
-rm -rf m-noobz
-rm -rf m-vmess
-rm -rf m-vless
-rm -rf m-trojan
-rm -rf m-system
-rm -rf m-sshovpn
-rm -rf m-ssws
-rm -rf running
-rm -rf m-backup
-rm -rf m-theme
-rm -rf m-ip
-rm -rf m-bot
-rm -rf update
-rm -rf ws-dropbear
-rm -rf bckpbot
-rm -rf tendang
-rm -rf bottelegram
-rm -rf restore
-rm -rf backup
-rm -rf cleaner
-rm -rf m-allxray
-rm -rf xraylimit
-rm -rf trialvmess
-rm -rf trialvless
-rm -rf trialtrojan
-rm -rf trialssh
-rm -rf autocpu
-rm -rf bantwidth
-rm -rf xp
-
+red() { echo -e "\\033[32;1m${*}\\033[0m"; }
+clear
 fun_bar() {
     CMD[0]="$1"
     CMD[1]="$2"
@@ -103,7 +22,7 @@ fun_bar() {
     echo -ne "  \033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["
     while true; do
         for ((i = 0; i < 18; i++)); do
-            echo -ne "\033[0;32m#"
+            echo -ne "\033[0;32m# "
             sleep 0.1s
         done
         [[ -e $HOME/fim ]] && rm $HOME/fim && break
@@ -117,24 +36,33 @@ fun_bar() {
     tput cnorm
 }
 res1() {
-wget https://raw.githubusercontent.com/oktaviaps/autoscript/main/Cdy/menu.zip
-wget -q -O /usr/bin/enc "https://raw.githubusercontent.com/oktaviaps/autoscript/main/Enc/encrypt" ; chmod +x /usr/bin/enc
-7z x -pInstal menu.zip
-chmod +x menu/*
-enc menu/*
-mv menu/* /usr/bin
-rm -rf menu
-rm -rf menu.zip
-rm -rf *.sh*
-rm -rf /usr/bin/*~
-rm -rf /usr/bin/gz*
-rm -rf /usr/bin/*.bak
-clear
+    wget https://raw.githubusercontent.com/oktaviaps/autoscript/main/Cdy/menu.zip
+    wget -q -O /usr/bin/enc "https://raw.githubusercontent.com/oktaviaps/autoscript/main/Enc/encrypt" ; chmod +x /usr/bin/enc
+    7z x -pInstal menu.zip
+    chmod +x menu/*
+    enc menu/*
+    mv menu/* /usr/bin
+    rm -rf menu
+    rm -rf menu.zip
+    rm -rf *.sh*
+    rm -rf /usr/bin/*~
+    rm -rf /usr/bin/gz*
+    rm -rf /usr/bin/*.bak
+#    rm -rf /usr/local/sbin/m-noobz
+#    wget /usr/local/sbin "https://raw.githubusercontent.com/oktaviaps/autoscript/main/Cfg/m-noobz" 
+#    cp m-noobz /usr/local/sbin
+#    rm m-noobz*
+#    chmod +x /usr/local/sbin/m-noobz
 }
+netfilter-persistent
+clear
+echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e " \e[1;97;101m          UPDATE SCRIPT        \e[0m"
+echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
-echo -e "  \033[1;91m Update Script...\033[1;37m"
+echo -e "  \033[1;91m update script service\033[1;37m"
 fun_bar 'res1'
-
+echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
-cd
+read -n 1 -s -r -p "Tekan [ Enter ] untuk kembali menu"
 menu
